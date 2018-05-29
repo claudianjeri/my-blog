51138fe29084f7c6808970d44f174c960844db1d
claudianjeri <claudianjeri04@gmail.com>
2018-05-29 16:40:04 +0300

    registering new users

--- app/models.py
+++ app/models.py
@@ -1,11 +1,17 @@
-from . import db
+from . import db,login_manager
 from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
 
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(db.Model, UserMixin):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key=True)
     username = db.Column(db.String(255))
     name = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique=True,index=True)
     pass_secure = db.Column(db.String(255))
 
     @property
