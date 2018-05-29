5f309c11a4262abed331f97b83f3f483cd265677
claudianjeri <claudianjeri04@gmail.com>
2018-05-29 16:19:14 +0300

    blueprints for authentication

--- app/__init__.py
+++ app/__init__.py
@@ -13,10 +13,13 @@ def create_app(config_state):
     app.config.from_object(config_options[config_state])
 
 
+
     bootstrap.init_app(app)
     db.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
+     from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
     return app
